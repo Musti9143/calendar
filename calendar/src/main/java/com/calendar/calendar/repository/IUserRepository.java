20c3970c87f012a180f2e639508cbd5b5fa6885e
HH Ozan <h.hueseyinozan@hotmail.de>
2024-05-02 20:24:42 +0200

    implement review comments

--- calendar/src/main/java/com/calendar/calendar/repository/IUserRepository.java
+++ calendar/src/main/java/com/calendar/calendar/repository/IUserRepository.java
@@ -1,9 +1,11 @@
-package com.calendar.calendar.entities.repository;
+package com.calendar.calendar.repository;
 
 import com.calendar.calendar.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface IUserRepository extends JpaRepository<User, UUID> {
 }
