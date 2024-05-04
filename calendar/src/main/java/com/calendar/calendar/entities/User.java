1957c07ea246dfe3dc9d0da8ac02207a3bdf5e3f
Musti9143 <mustafaduezdemir@hotmail.com>
2024-05-04 20:28:33 +0200

    change user entity generationtype to AUTO

--- calendar/src/main/java/com/calendar/calendar/entities/User.java
+++ calendar/src/main/java/com/calendar/calendar/entities/User.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 public class User implements Serializable{
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
     @Nonnull
     private String name;
