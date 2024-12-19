a163253674f2a4ff0b59ef561203316dc4c10c0d
mma <mma@hotmail.de>
2024-12-19 21:27:06 +0100

    move files

--- calendar/src/main/java/com/calendar/calendar/dto/AppointmentDTO.java
+++
@@ -1,5 +0,0 @@
-package com.calendar.calendar.dto;
-
-import java.sql.Timestamp;
-
-public record AppointmentDTO(String title, String author, Timestamp startDateTime, Timestamp endDateTime, String description) { }
--- calendar/src/main/java/com/calendar/calendar/dto/UserDTO.java
+++
@@ -1,3 +0,0 @@
-package com.calendar.calendar.dto;
-
-public record UserDTO(String name, String surname, String email) { }
