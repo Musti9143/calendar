e74b56a7cd507e57f909b3f46bd7e82aa9c990d9
Musti9143 <mustafaduezdemir@hotmail.com>
2025-01-20 16:43:17 +0100

    43: change argument naming of AppointmentResponse author to email

--- src/main/java/com/calendar/communication/out/AppointmentResponse.java
+++ src/main/java/com/calendar/communication/out/AppointmentResponse.java
@@ -2,4 +2,4 @@ package com.calendar.communication.out;
 
 import java.sql.Timestamp;
 
-public record AppointmentResponse(String title, String author, Timestamp startDateTime, Timestamp endDateTime, String description) { }
+public record AppointmentResponse(String title, String email, Timestamp startDateTime, Timestamp endDateTime, String description) { }
