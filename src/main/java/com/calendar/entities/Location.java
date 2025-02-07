bc537c9c34c02a1237c052e7926855d801c1e394
Musti9143 <mustafaduezdemir@hotmail.com>
2025-02-08 00:21:37 +0100

    60: refactor

--- src/main/java/com/calendar/entities/Location.java
+++ src/main/java/com/calendar/entities/Location.java
@@ -5,16 +5,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Embeddable
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Location{
+public class Location implements Serializable {
 
     private String street;
     private String streetNumber;
     private String postalCode;
     private String city;
     private String country;
-
 }
